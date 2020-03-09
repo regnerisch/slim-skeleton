f62874ea710e48e5269ebd7ea7a0d610a8c1edf7
Jonas Regner <jonas.regner@dotsunited.de>
2020-03-09 15:18:25 +0100

    update return type

--- app/ServiceProviders/AppServiceProvider.php
+++ app/ServiceProviders/AppServiceProvider.php
@@ -5,16 +5,17 @@ declare(strict_types=1);
 namespace App\ServiceProviders;
 
 use DI\ContainerBuilder;
+use Psr\Container\ContainerInterface;
 use Slim\App;
 
 class AppServiceProvider
 {
-    public function registerContainer()
+    public function registerContainer(): ContainerInterface
     {
         return (new ContainerServiceProvider())(new ContainerBuilder());
     }
 
-    public function register(App $app)
+    public function register(App $app): void
     {
         (new MiddlewareServiceProvider())($app);
         (new RouteServiceProvider())($app);
